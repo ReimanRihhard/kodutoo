9c37e27b850a0ce132dea4441a9b86042978ca5b
ReimanRihhard <144675895+ReimanRihhard@users.noreply.github.com>
2023-10-09 13:30:14 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,18 +1,18 @@
 function checkTemp(i){
 
     if (i <= 20 && i > 0){
-    console.log(-1)
+       return -1
    
     } else if (i <= 40 && i > 0){
-        console.log(0)
+        return 0
    
     } else if (i <= 60 && i > 0){
-        console.log(1)
+         return 1
    
     } else if (i > 60 && i > 0){
-        console.log("Temperatuur liiga suur")
+        return "Temperatuur on liiga suur"
     }  else {
-       console.log("Temperatuur on miinuses")
+       return "Temperatuur on miinuses"
     }
 }
 
