ae31babbe4a354c5a1a59d7043368bd90e398ff3
ReimanRihhard <144675895+ReimanRihhard@users.noreply.github.com>
2023-10-09 12:56:24 +0300

    Create checkTemp.js

---
+++ checkTemp.js
@@ -0,0 +1,20 @@
+let i = parseFloat(prompt("Sisesta temperatuur"))
+
+function tempCheck() {
+   
+
+    if (i <= 20){
+    console.log(-1)
+   
+    } else if (i <= 40){
+        console.log(0)
+   
+    } else if (i <= 60){
+        console.log(1)
+   
+    } else {
+        console.log("Temperatuur on miinuses I I Temperatuur liiga suur")
+    }
+}
+
+tempCheck()
