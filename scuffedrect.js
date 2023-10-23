386d341fbbd5a56e206b2af81d1e7f0115050be5
ReimanRihhard <144675895+ReimanRihhard@users.noreply.github.com>
2023-10-23 14:59:53 +0300

    Create scuffedrect.js

---
+++ scuffedrect.js
@@ -0,0 +1,15 @@
+function hollowRect(w,h){
+    read = []
+    for (i = 0; i<w; i++){
+     for (j = 0; j<h; j++){    
+       read.push('*')
+    }
+        read.push('\n')
+    }
+    let output = (read.join(""))
+    console.log(output)
+    
+}
+
+rect(7,8)
+
